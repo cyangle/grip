30b16101dfcf84a44db9d6916dbe424cc904e80b
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2025-06-30 11:40:49 +0400

    Fixed typo

--- src/grip/exceptions/bad_request.cr
+++ src/grip/exceptions/bad_request.cr
@@ -2,7 +2,7 @@ module Grip
   module Exceptions
     class BadRequest < Base
       def initialize(message : String? = nil)
-        @@status_code = HTTP::Status::BAD_REQUEST
+        @status_code = HTTP::Status::BAD_REQUEST
         @message = message if message
         @message = "Please provide a proper request to the endpoint." unless message
       end
